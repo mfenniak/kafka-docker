ba31374499bb77fd55478c4f6e07e1457ac0ec69
Mathieu Fenniak <mathieu.fenniak@replicon.com>
2017-04-10 07:54:27 -0600

    Incorporate Kafka 0.10.2.1-RC0

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+# mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+# url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+url="http://home.apache.org/~gwenshap/kafka-0.10.2.1-rc0/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
